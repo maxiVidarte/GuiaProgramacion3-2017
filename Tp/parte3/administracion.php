9161e856b8a26253ec63881560866daad8bb5877
maxiVidarte <maxiavidarte@gmail.com>
2017-04-16 23:54:40 -0300

    cambios

--- Tp/parte3/administracion.php
+++ Tp/parte3/administracion.php
@@ -2,7 +2,18 @@
 include "Empleado.php";
 
 if(isset($_POST["guardar"]))
+//validar que el archivo es una imagen
+echo $_FILES['archivo']['name']."</br>";
+//validar solo las extensiones jpg, bmp, gif, png o jpeg2
+echo $_FILES['archivo']['type']."</br>";
+//validar que el tamaño sea menor a 1MB(1024.000)
+echo $_FILES['archivo']['size']."</br>";
+//validar que no exista un archivo con el mismo nombre en el repositorio final (./fotog)
+
+
+
 $empleado= new Empleado($_POST["nombre"],$_POST["apellido"],$_POST["dni"],$_POST["sexo"],$_POST["legajo"],$_POST["sueldo"]);
+//$empleado->setPathFoto($_POST["archivo"]);
 $path = "empleados.txt";
     if(isset($_POST["nombre"],$_POST["apellido"],$_POST["dni"],$_POST["sexo"],$_POST["legajo"],$_POST["sueldo"]))
     {
